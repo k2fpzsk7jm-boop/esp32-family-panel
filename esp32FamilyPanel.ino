d7d2352e3cf3e4d17752a474650537cdc02647d8
jrowe <k2fpzsk7jm@privaterelay.appleid.com>
2026-08-04 20:14:44 -0400

    Update esp32FamilyPanel.ino

--- esp32FamilyPanel.ino
+++ esp32FamilyPanel.ino
@@ -1,1 +1,21 @@
+/*
+  ESP32 Family Panel
 
+  Main application entry point.
+
+  Hardware:
+  Elecrow CrowPanel Advance 7"
+  Model: DIS02170A
+
+*/
+
+void setup()
+{
+    // System initialization will be added here
+}
+
+
+void loop()
+{
+    // Main application update loop
+}
